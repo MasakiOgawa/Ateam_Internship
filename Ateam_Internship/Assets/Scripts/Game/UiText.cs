89ff3a9f78ec064d4e89b0611c24642a4cc7d9b3
DESKTOP-HB6JSN7\miyu <picklesss.common@gmail.com>
2017-09-26 03:01:45 +0900

    UI

---
+++ Ateam_Internship/Assets/Scripts/Game/UiText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiText : MonoBehaviour {
+
+    // メンバ変数
+    private bool bSetUi;
+    private GameManager gameManager;
+
+    public Image image;
+
+    // Use this for initialization
+    void Start ()
+    {
+        image.enabled = false;
+        bSetUi = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UIがセットされてたら
+        if (bSetUi == true)
+        {
+            // プレイヤーのターン
+            if (gameManager.GetGameState() == DEFINE.GAME_STATE.PLAYER_TURN)
+            {
+                image.enabled = true;
+                
+                //PuzzlePiece[Cnt].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            // エネミーのターン
+            else if (gameManager.GetGameState() == DEFINE.GAME_STATE.ENEMY_TURN)
+            {
+                image.enabled = true;
+            }
+
+            bSetUi = false;
+        }
+    }
+        
+    // UI情報を設定
+    public void SetUI(bool setUi)
+    {
+        bSetUi = setUi;
+    }
+
+    // UI情報を取得
+    public bool GetUi()
+    {
+        return bSetUi;
+    }
+}
